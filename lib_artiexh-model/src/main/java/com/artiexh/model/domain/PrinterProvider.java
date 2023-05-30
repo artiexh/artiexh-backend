de4eac096b24b7b2c25c7e598a470a400afdbfa1
tackedev <quangky.it@gmail.com>
2023-05-31 00:01:38 +0700

    add printer provider table

---
+++ lib_artiexh-model/src/main/java/com/artiexh/model/domain/PrinterProvider.java
@@ -0,0 +1,16 @@
+package com.artiexh.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class PrinterProvider extends Account {
+	private String name;
+}
