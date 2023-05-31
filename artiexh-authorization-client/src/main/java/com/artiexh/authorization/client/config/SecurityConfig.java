6f6f146cbd53eacc7e94ee1888c478d820e1e6de
tackedev <quangky.it@gmail.com>
2023-06-01 00:21:29 +0700

    AR-06 update security config for artiexh-api

--- artiexh-authorization-client/src/main/java/com/artiexh/authorization/client/config/SecurityConfig.java
+++ artiexh-authorization-client/src/main/java/com/artiexh/authorization/client/config/SecurityConfig.java
@@ -28,7 +28,6 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity,
-		/*AuthenticationManager authenticationManager,*/
 										   JwtAuthenticationFilter jwtAuthenticationFilter,
 										   HttpCookieOAuth2AuthorizationRequestRepository authorizationRequestRepository,
 										   ArtiexhOAuth2UserService artiexhOAuth2UserService,
@@ -40,7 +39,6 @@ public class SecurityConfig {
 										   JwtAuthenticationProvider jwtAuthenticationProvider) throws Exception {
 		httpSecurity.authenticationProvider(jwtAuthenticationProvider);
 		httpSecurity
-			//.authenticationManager(authenticationManager)
 			.addFilterAt(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 			.csrf(CsrfConfigurer::disable)
 			.cors(Customizer.withDefaults())
