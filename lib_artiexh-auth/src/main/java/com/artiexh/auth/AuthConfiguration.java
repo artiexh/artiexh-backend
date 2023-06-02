b57583dd89c6cf8f482a09e7b3dd52f7c4bfa181
Le Quang Ky <74134498+tackedev@users.noreply.github.com>
2023-06-02 09:55:44 +0700

    Ar 6 printer provider registration (#5)
    * AR-06 update security config for artiexh-api
    
    * AR-06 add register printer provider flow
    
    * AR-06 add get me
    
    * AR-06 update auth with AccountEntity instead of UserEntity
    
    * AR-06 add allow cors localhost:3000
    
    * AR-06 cors config add allow all headers
    
    * AR-06 cors add allowCredentials and fix check null password in userMapper
    
    * AR-06 resolve conflict

--- lib_artiexh-auth/src/main/java/com/artiexh/auth/AuthConfiguration.java
+++ lib_artiexh-auth/src/main/java/com/artiexh/auth/AuthConfiguration.java
@@ -32,6 +32,8 @@ public class AuthConfiguration {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
