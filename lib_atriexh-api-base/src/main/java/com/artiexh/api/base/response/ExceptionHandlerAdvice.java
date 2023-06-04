fac2d96320de96c69a7b37ece10f5cc1aa5f7aac
tackedev <quangky.it@gmail.com>
2023-06-05 00:14:52 +0700

    fix wrong error response

--- lib_atriexh-api-base/src/main/java/com/artiexh/api/base/response/ExceptionHandlerAdvice.java
+++ lib_atriexh-api-base/src/main/java/com/artiexh/api/base/response/ExceptionHandlerAdvice.java
@@ -2,51 +2,19 @@ package com.artiexh.api.base.response;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.time.Instant;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
-
-	@Override
-	protected ResponseEntity<Object> createResponseEntity(Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
-		ResponseModel responseModel;
-
-		String path = null;
-		if (request instanceof ServletWebRequest servletWebRequest) {
-			path = servletWebRequest.getRequest().getRequestURI();
-		}
-
-		if (body instanceof ProblemDetail problemDetail) {
-			responseModel = new ResponseModel(
-				Instant.now(),
-				statusCode.value(),
-				((HttpStatus) statusCode).name(),
-				problemDetail.getDetail(),
-				path,
-				null
-			);
-		} else {
-			responseModel = new ResponseModel(
-				Instant.now(),
-				statusCode.value(),
-				((HttpStatus) statusCode).name(),
-				body.toString(),
-				path,
-				null
-			);
-		}
-
-		return new ResponseEntity<>(responseModel, headers, statusCode);
-	}
 
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
