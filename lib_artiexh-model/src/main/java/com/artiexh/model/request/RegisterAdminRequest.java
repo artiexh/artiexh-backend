354552dad844c506614f1f0960df35c3c1e79c46
tackedev <quangky.it@gmail.com>
2023-06-06 16:45:46 +0700

    add validation for request

--- lib_artiexh-model/src/main/java/com/artiexh/model/request/RegisterAdminRequest.java
+++ lib_artiexh-model/src/main/java/com/artiexh/model/request/RegisterAdminRequest.java
@@ -1,6 +1,9 @@
 package com.artiexh.model.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,14 +14,20 @@ import lombok.NoArgsConstructor;
 public class RegisterAdminRequest {
 
 	@NotBlank(message = "Username is required")
+	@Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+	@Pattern(regexp = "^[^\\s+]*$", message = "Username must be exclude whitespaces")
 	private String username;
 
 	@NotBlank(message = "Password is required")
+	@Size(min = 8, max = 72, message = "Username must be between 8 and 72 characters")
+	@Pattern(regexp = "^[^\\s+]*$", message = "Password must be exclude whitespaces")
 	private String password;
 
 	@NotBlank(message = "Display name is required")
+	@Size(min = 4, max = 50, message = "Display name must be between 4 and 50 characters")
 	private String displayName;
 
+	@Email
 	private String email;
 
 }
