d916da9d9f21eedc7e159c1160c58f1814c4f152
Le Quang Ky <74134498+tackedev@users.noreply.github.com>
2023-06-08 23:45:17 +0700

    fix validate password msg (#12)

--- lib_atriexh-api-base/src/main/java/com/artiexh/api/base/response/ExceptionHandlerAdvice.java
+++ lib_atriexh-api-base/src/main/java/com/artiexh/api/base/response/ExceptionHandlerAdvice.java
@@ -8,16 +8,18 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.Instant;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntityNotFoundException.class)
