b9b44b121d6b2975ff3cef2c1da4f0f2a6c9e85c
Nguyễn Thanh Hà <nthoreoo2602@gmail.com>
2023-06-11 21:57:29 +0700

    Fix error code and mapper

--- artiexh-api/src/main/java/com/artiexh/api/service/impl/AccountServiceImpl.java
+++ artiexh-api/src/main/java/com/artiexh/api/service/impl/AccountServiceImpl.java
@@ -7,6 +7,7 @@ import com.artiexh.data.jpa.entity.UserEntity;
 import com.artiexh.data.jpa.repository.AccountRepository;
 import com.artiexh.model.domain.Account;
 import com.artiexh.model.domain.Role;
+import com.artiexh.model.mapper.AccountMapper;
 import com.artiexh.model.mapper.ArtistMapper;
 import com.artiexh.model.mapper.PrinterProviderMapper;
 import com.artiexh.model.mapper.UserMapper;
@@ -19,13 +20,14 @@ public class AccountServiceImpl implements AccountService {
 	private final AccountRepository accountRepository;
 	private final UserMapper userMapper;
 	private final ArtistMapper artistMapper;
+	private final AccountMapper accountMapper;
 	private final PrinterProviderMapper printerProviderMapper;
 
 	@Override
 	public Account getUserById(Long id) {
 		return accountRepository.findById(id)
 			.map(accountEntity -> switch (Role.fromValue(accountEntity.getRole())) {
-				case ADMIN -> null;
+				case ADMIN -> accountMapper.entityToDomain(accountEntity);
 				case USER -> userMapper.entityToDomain((UserEntity) accountEntity);
 				case ARTIST -> artistMapper.entityToDomain((ArtistEntity) accountEntity);
 				case PRINTER_PROVIDER -> printerProviderMapper.entityToDomain((PrinterProviderEntity) accountEntity);
