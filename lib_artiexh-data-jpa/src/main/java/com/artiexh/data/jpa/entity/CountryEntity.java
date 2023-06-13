110982e75ba0482505201e770353e5fad86c9a37
tackedev <quangky.it@gmail.com>
2023-06-13 10:33:52 +0700

    add artist province

---
+++ lib_artiexh-data-jpa/src/main/java/com/artiexh/data/jpa/entity/CountryEntity.java
@@ -0,0 +1,30 @@
+package com.artiexh.data.jpa.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "country")
+public class CountryEntity {
+
+	@Id
+	@Column(name = "id", nullable = false)
+	private Short id;
+
+	@Size(max = 255)
+	@NotNull
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@OneToMany(mappedBy = "country")
+	private Set<ProvinceEntity> provinces = new LinkedHashSet<>();
+
+}
--- lib_artiexh-model/src/main/java/com/artiexh/model/mapper/ArtistMapper.java
+++ lib_artiexh-model/src/main/java/com/artiexh/model/mapper/ArtistMapper.java
@@ -1,9 +1,13 @@
 package com.artiexh.model.mapper;
 
 import com.artiexh.data.jpa.entity.ArtistEntity;
+import com.artiexh.data.jpa.entity.ProvinceEntity;
 import com.artiexh.model.domain.Artist;
 import com.artiexh.model.domain.ArtistInfo;
+import com.artiexh.model.domain.Province;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(
@@ -12,10 +16,15 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 )
 public interface ArtistMapper {
 
+	@Mapping(target = "province", qualifiedByName = "provinceEntityToDomain")
 	Artist entityToDomain(ArtistEntity userEntity);
 
 	ArtistEntity domainToEntity(Artist user);
 
 	ArtistInfo entityToInfo(ArtistEntity entity);
+
+	@Named("provinceEntityToDomain")
+	@Mapping(target = "country.provinces", ignore = true)
+	Province provinceEntityToDomain(ProvinceEntity provinceEntity);
 
 }
