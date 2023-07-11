c508b6a4eca7eb2e247bc84b9bb876ea748bc368
Nguyễn Thanh Hà <nthoreoo2602@gmail.com>
2023-07-11 22:25:58 +0700

    Update query

--- artiexh-api/src/main/java/com/artiexh/api/controller/ArtistController.java
+++ artiexh-api/src/main/java/com/artiexh/api/controller/ArtistController.java
@@ -1,2 +1,36 @@
-package com.artiexh.api.controller;public class ArtistController {
+package com.artiexh.api.controller;
+
+import com.artiexh.api.base.common.Endpoint;
+import com.artiexh.api.service.ArtistService;
+import com.artiexh.model.rest.PageResponse;
+import com.artiexh.model.rest.PaginationAndSortingRequest;
+import com.artiexh.model.rest.artist.filter.ProductPageFilter;
+import com.artiexh.model.rest.product.request.GetAllProductFilter;
+import com.artiexh.model.rest.product.response.ProductResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(Endpoint.Artist.ROOT)
+public class ArtistController {
+	private final ArtistService artistService;
+
+	@GetMapping(Endpoint.Artist.ARTIST_PRODUCT)
+	@PreAuthorize("hasAuthority('ARTIST')")
+	public PageResponse<ProductResponse> getAllProduct (
+		Authentication authentication,
+		@ParameterObject @Valid PaginationAndSortingRequest paginationAndSortingRequest,
+		@ParameterObject @Valid ProductPageFilter filter
+	) {
+		long userId = (long) authentication.getPrincipal();
+		filter.setArtistId(userId);
+		return artistService.getAllProducts(filter.getQuery(), paginationAndSortingRequest.getPageable());
+	}
 }
--- artiexh-api/src/main/java/com/artiexh/api/service/ArtistService.java
+++ artiexh-api/src/main/java/com/artiexh/api/service/ArtistService.java
@@ -1,2 +1,10 @@
-package com.artiexh.api.service;public interface ArtistService {
+package com.artiexh.api.service;
+
+import com.artiexh.model.rest.PageResponse;
+import com.artiexh.model.rest.product.response.ProductResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.query.Query;
+
+public interface ArtistService {
+	PageResponse<ProductResponse> getAllProducts (Query query, Pageable pageable);
 }
