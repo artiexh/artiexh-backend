d4cb3a32cc741402f2dc6ce56e3a5c5797912fe0
Nguyễn Thanh Hà <80661424+NTH260202@users.noreply.github.com>
2023-08-18 21:37:05 +0700

    Update category and tag (#29)

--- lib_artiexh-model/src/main/java/com/artiexh/model/mapper/ProductTagMapper.java
+++ lib_artiexh-model/src/main/java/com/artiexh/model/mapper/ProductTagMapper.java
@@ -2,6 +2,7 @@ package com.artiexh.model.mapper;
 
 import com.artiexh.data.jpa.entity.ProductTagEntity;
 import com.artiexh.model.domain.ProductTag;
+import com.artiexh.model.rest.tag.ProductTagResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -14,6 +15,8 @@ public interface ProductTagMapper {
 	ProductTag entityToDomain(ProductTagEntity productTagEntity);
 
 	ProductTagEntity domainToEntity(ProductTag productTag);
+
+	ProductTagResponse entityToResponse(ProductTagEntity productTag);
 
 	default String tagDomainToTagResponse(ProductTag tag) {
 		if (tag == null) {
