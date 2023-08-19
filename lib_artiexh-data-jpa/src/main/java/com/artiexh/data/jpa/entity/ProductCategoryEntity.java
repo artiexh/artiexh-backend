e99cd60bbccb9862bbf431fc71a95105a58465d4
Nguyễn Thanh Hà <nthoreoo2602@gmail.com>
2023-08-19 23:31:09 +0700

    Fix cart item id and update categpry thumnail

--- lib_artiexh-data-jpa/src/main/java/com/artiexh/data/jpa/entity/ProductCategoryEntity.java
+++ lib_artiexh-data-jpa/src/main/java/com/artiexh/data/jpa/entity/ProductCategoryEntity.java
@@ -24,4 +24,6 @@ public class ProductCategoryEntity {
 	@Column(name = "name", nullable = false)
 	private String name;
 
+	@Column(name = "image_url", nullable = false)
+	private String imageUrl;
 }
