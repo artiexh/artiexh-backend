4bf893abef0556de6ce782212053a84f2215e75a
tackedev <quangky.it@gmail.com>
2023-08-21 12:51:07 +0700

    fix swagger server

--- lib_atriexh-api-base/src/main/java/com/artiexh/api/base/config/SpringDocConfig.java
+++ lib_atriexh-api-base/src/main/java/com/artiexh/api/base/config/SpringDocConfig.java
@@ -2,6 +2,7 @@ package com.artiexh.api.base.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -11,7 +12,9 @@ public class SpringDocConfig {
 
 	@Bean
 	public OpenAPI openAPI(Environment environment) {
-		return new OpenAPI().info(new Info().title(environment.getProperty("spring.application.name")).version("v1"));
+		return new OpenAPI()
+			.addServersItem(new Server().url("/"))
+			.info(new Info().title(environment.getProperty("spring.application.name")).version("v1"));
 	}
 
 }
