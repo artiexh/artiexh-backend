cf8ee3ec34ed4a0ae8928811dca227acf4bb5b90
Nguyễn Thanh Hà <80661424+NTH260202@users.noreply.github.com>
2023-08-22 18:13:16 +0700

    Fix error status (#46)

--- artiexh-api/src/main/java/com/artiexh/api/controller/AccountController.java
+++ artiexh-api/src/main/java/com/artiexh/api/controller/AccountController.java
@@ -1,14 +1,17 @@
 package com.artiexh.api.controller;
 
 import com.artiexh.api.base.common.Endpoint;
+import com.artiexh.api.exception.ErrorCode;
 import com.artiexh.api.service.AccountService;
 import com.artiexh.model.domain.Account;
 import com.artiexh.model.domain.Role;
 import com.artiexh.model.rest.account.AccountProfile;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,7 +28,12 @@ public class AccountController {
 
 	@GetMapping(value = Endpoint.Account.PROFILE)
 	public AccountProfile getProfile(@PathVariable Long id) {
-		return accountService.getUserProfile(id);
+		try {
+			return accountService.getUserProfile(id);
+		} catch (EntityNotFoundException ex) {
+			throw new ResponseStatusException(ErrorCode.USER_NOT_FOUND.getCode(), ErrorCode.USER_NOT_FOUND.getMessage(), ex);
+		}
+
 	}
 
 }
