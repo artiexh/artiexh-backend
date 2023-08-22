053415fc3fb14c15bcb8d850b43ce16d15a8b4b0
Nguyễn Thanh Hà <nthoreoo2602@gmail.com>
2023-08-22 17:51:40 +0700

    Resolve conflict

--- lib_artiexh-data-jpa/src/main/java/com/artiexh/data/jpa/repository/ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.artiexh.data.jpa.repository;
-
-import com.artiexh.data.jpa.entity.ShopEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShopRepository extends JpaRepository<ShopEntity, Long> {
-}
