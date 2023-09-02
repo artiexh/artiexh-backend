e42708c218e224a6f77366075423e799e7ef243f
Nguyễn Thanh Hà <80661424+NTH260202@users.noreply.github.com>
2023-09-03 01:57:27 +0700

    Update provider product page (#87)
    * Update provider product page
    
    * Fix timezone

--- artiexh-api/src/main/java/com/artiexh/api/service/impl/ProvidedProductBaseServiceImpl.java
+++ artiexh-api/src/main/java/com/artiexh/api/service/impl/ProvidedProductBaseServiceImpl.java
@@ -11,6 +11,9 @@ import com.artiexh.model.domain.ProvidedProductBase;
 import com.artiexh.model.mapper.ProvidedProductBaseMapper;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -62,5 +65,11 @@ public class ProvidedProductBaseServiceImpl implements ProvidedProductBaseServic
 			.build())
 			.orElseThrow(EntityNotFoundException::new);
 		return mapper.entityToDomain(entity);
+	}
+
+	@Override
+	public Page<ProvidedProductBase> getAll(Specification<ProvidedProductBaseEntity> specification, Pageable pageable) {
+		Page<ProvidedProductBaseEntity> productPage = repository.findAll(specification, pageable);
+		return productPage.map(mapper::entityToDomain);
 	}
 }
