99b5fba804e8a2b3d81405d6c12329d9368eb208
Nguyễn Thanh Hà <nthoreoo2602@gmail.com>
2023-09-13 20:31:43 +0700

    Fix id response

--- lib_artiexh-model/src/main/java/com/artiexh/model/domain/VariantCombination.java
+++ lib_artiexh-model/src/main/java/com/artiexh/model/domain/VariantCombination.java
@@ -1,5 +1,8 @@
 package com.artiexh.model.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VariantCombination {
+	@JsonSerialize(using = ToStringSerializer.class)
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	private Long variantId;
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Long optionId;
+	@JsonSerialize(using = ToStringSerializer.class)
 	private Long optionValueId;
 	private Integer quantity;
 }
