17358b9712ad8a758c2b9e6a04c407070a131b7c
tackedev <quangky.it@gmail.com>
2023-09-18 22:25:07 +0700

    add proxy demo

--- artiexh-api/src/main/java/com/artiexh/api/service/impl/AddressServiceImpl.java
+++ artiexh-api/src/main/java/com/artiexh/api/service/impl/AddressServiceImpl.java
@@ -1,61 +1,69 @@
 package com.artiexh.api.service.impl;
 
 import com.artiexh.api.service.AddressService;
-import com.artiexh.data.jpa.repository.CountryRepository;
-import com.artiexh.data.jpa.repository.DistrictRepository;
-import com.artiexh.data.jpa.repository.ProvinceRepository;
-import com.artiexh.data.jpa.repository.WardRepository;
 import com.artiexh.model.domain.Country;
 import com.artiexh.model.domain.District;
 import com.artiexh.model.domain.Province;
 import com.artiexh.model.domain.Ward;
-import com.artiexh.model.mapper.AddressMapper;
-import com.artiexh.model.mapper.ProvinceMapper;
 import com.artiexh.model.rest.address.ProvinceResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
+// Proxy class
 public class AddressServiceImpl implements AddressService {
-	private final CountryRepository countryRepository;
-	private final ProvinceRepository provinceRepository;
-	private final DistrictRepository districtRepository;
-	private final WardRepository wardRepository;
-	private final ProvinceMapper provinceMapper;
-	private final AddressMapper addressMapper;
+	@Qualifier("jpaAddressService")
+	private final AddressService jpaAddressService;
+	@Qualifier("redisAddressService")
+	private final AddressService redisAddressService;
 
 	@Override
 	public Page<ProvinceResponse> getInPage(Pageable pageable) {
-		return provinceRepository.findAll(pageable).map(provinceMapper::entityToResponse);
+		var result = redisAddressService.getInPage(pageable);
+		if (result.isEmpty()) {
+			result = jpaAddressService.getInPage(pageable);
+		}
+		return result;
 	}
 
 	@Override
 	public Page<Country> getCountries(Pageable pageable) {
-		return countryRepository.findAll(pageable)
-			.map(addressMapper::countryEntityToCountry);
+		var result = redisAddressService.getCountries(pageable);
+		if (result.isEmpty()) {
+			result = jpaAddressService.getCountries(pageable);
+		}
+		return result;
 	}
 
 	@Override
 	public Page<Province> getProvinces(Short countryId, Pageable pageable) {
-		return provinceRepository.findAllByCountryId(countryId, pageable)
-			.map(addressMapper::provinceEntityToProvince);
+		var result = redisAddressService.getProvinces(countryId, pageable);
+		if (result.isEmpty()) {
+			result = jpaAddressService.getProvinces(countryId, pageable);
+		}
+		return result;
 	}
 
 	@Override
 	public Page<District> getDistricts(Short provinceId, Pageable pageable) {
-		return districtRepository.findAllByProvinceId(provinceId, pageable)
-			.map(addressMapper::districtEntityToDistrict);
+		var result = redisAddressService.getDistricts(provinceId, pageable);
+		if (result.isEmpty()) {
+			result = jpaAddressService.getDistricts(provinceId, pageable);
+		}
+		return result;
 	}
 
 	@Override
 	public Page<Ward> getWards(Short districtId, Pageable pageable) {
-		return wardRepository.findAllByDistrictId(districtId, pageable)
-			.map(addressMapper::wardEntityToWard);
+		var result = redisAddressService.getWards(districtId, pageable);
+		if (result.isEmpty()) {
+			result = jpaAddressService.getWards(districtId, pageable);
+		}
+		return result;
 	}
 
 }
