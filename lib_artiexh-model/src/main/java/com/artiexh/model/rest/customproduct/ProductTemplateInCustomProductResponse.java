cdb39e86920b2521aedfa43657bea4c5acada458
tackedev <quangky.it@gmail.com>
2023-11-08 00:41:32 +0700

    add campaign variant product template detail info

--- lib_artiexh-model/src/main/java/com/artiexh/model/rest/customproduct/ProductTemplateInCustomProductResponse.java
+++ lib_artiexh-model/src/main/java/com/artiexh/model/rest/customproduct/ProductTemplateInCustomProductResponse.java
@@ -1,14 +1,25 @@
 package com.artiexh.model.rest.customproduct;
 
+import com.artiexh.data.jpa.entity.embededmodel.ImageCombination;
+import com.artiexh.data.jpa.entity.embededmodel.OptionConfig;
+import com.artiexh.model.domain.Model3DCode;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
 public class ProductTemplateInCustomProductResponse {
 	@JsonSerialize(using = ToStringSerializer.class)
 	private Long id;
+	private String code;
 	private String name;
+	@JsonSerialize(using = ToStringSerializer.class)
+	private Long modelFileId;
+	private List<OptionConfig> sizes;
+	private List<ImageCombination> imageCombinations;
+	private Model3DCode model3DCode;
 }
