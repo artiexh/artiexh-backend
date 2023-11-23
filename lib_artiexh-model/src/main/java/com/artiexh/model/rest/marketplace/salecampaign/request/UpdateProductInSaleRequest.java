fc15a593584ca5a42134203fa595de2586455751
tackedev <quangky.it@gmail.com>
2023-11-23 11:49:20 +0700

    auto set quantity to 0

--- lib_artiexh-model/src/main/java/com/artiexh/model/rest/marketplace/salecampaign/request/UpdateProductInSaleRequest.java
+++ lib_artiexh-model/src/main/java/com/artiexh/model/rest/marketplace/salecampaign/request/UpdateProductInSaleRequest.java
@@ -1,6 +1,8 @@
 package com.artiexh.model.rest.marketplace.salecampaign.request;
 
 import com.artiexh.model.domain.Money;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateProductInSaleRequest {
+	@NotNull
+	@Min(0)
 	private Integer quantity;
 	private Money price;
 	private BigDecimal artistProfit;
