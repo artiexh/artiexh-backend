bc9dba84a6a614013348a5e67487491b1c5ab12a
tackedev <quangky.it@gmail.com>
2023-12-11 00:08:20 +0700

    remove ws host

--- artiexh-api/src/main/java/com/artiexh/api/config/SocketIOConfig.java
+++ artiexh-api/src/main/java/com/artiexh/api/config/SocketIOConfig.java
@@ -1,11 +1,6 @@
 package com.artiexh.api.config;
 
-import com.artiexh.model.domain.NotificationMessage;
-import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DataListener;
-import com.corundumstudio.socketio.listener.DisconnectListener;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,15 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.annotation.PreDestroy;
-import java.util.List;
 
 @CrossOrigin
 @Configuration
 @Slf4j
 public class SocketIOConfig {
-
-	@Value("${socket.host}")
-	private String host;
 
 	@Value("${socket.port}")
 	private Integer port;
@@ -32,7 +23,6 @@ public class SocketIOConfig {
 	@Bean
 	public SocketIOServer socketIOServer() {
 		com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-		config.setHostname(host);
 		config.setPort(port);
 		server = new SocketIOServer(config);
 		server.start();
